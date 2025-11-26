ddbe8790827dd1ead068469337179122f9eab1e4
mattis1234567 <mattisvrager@gmail.com>
2025-11-26 13:42:55 +0200

    Remove validator errors and comment code

--- src/mäng1.js
+++ src/mäng1.js
@@ -1,61 +1,61 @@
 /**
- * Namespace
+ * Allikas/Autor: Caio Paiola (https://codepen.io/CaioPaiola/pen/nojJmQ)
  */
 var Game      = Game      || {};
 var Keyboard  = Keyboard  || {}; 
 var Component = Component || {};
 
 /**
- * Keyboard Map
+ * Klaviatuuri koodide ja suundade vastavus (Keyboard Map)
  */
 Keyboard.Keymap = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down'
+  37: 'left', // Vasak nooleklahv
+  38: 'up',   // Üles nooleklahv
+  39: 'right', // Parem nooleklahv
+  40: 'down'  // Alla nooleklahv
 };
 
 /**
- * Keyboard Events
+ * Klaviatuuri sündmuste kontroller
  */
 Keyboard.ControllerEvents = function() {
   
-  // Setts
+  // Seaded
   var self      = this;
-  this.pressKey = null;
+  this.pressKey = null; // Hoia viimati vajutatud klahvi koodi
   this.keymap   = Keyboard.Keymap;
   
-  // Keydown Event
+  // Klaviatuuri allavajutamise sündmus
   document.onkeydown = function(event) {
-    self.pressKey = event.which;
+    self.pressKey = event.which; // Salvesta vajutatud klahvi kood (event.which)
   };
   
-  // Get Key
+  // Tagastab suuna (nt 'left', 'up'), mis vastab vajutatud klahvile
   this.getKey = function() {
     return this.keymap[this.pressKey];
   };
 };
 
 /**
- * Game Component Stage
+ * Mängukomponendi Lava (Stage)
  */
 Component.Stage = function(canvas, conf) {  
   
-  // Sets
+  // Seaded
   this.keyEvent  = new Keyboard.ControllerEvents();
-  this.width     = canvas.width;
-  this.height    = canvas.height;
-  this.length    = [];
-  this.food      = {};
-  this.score     = 0;
-  this.direction = 'right';
+  this.width     = canvas.width;  // Lõuendi laius
+  this.height    = canvas.height; // Lõuendi kõrgus
+  this.length    = [];            // Ussi segmentide massiiv (keha pikkus)
+  this.food      = {};            // Toidu koordinaadid
+  this.score     = 0;             // Praegune skoor
+  this.direction = 'right';       // Ussi algsuund
   this.conf      = {
-    cw   : 10,
-    size : 5,
-    fps  : 1000
+    cw   : 10,  // Ühe ploki (lahtri) laius
+    size : 5,   // Ussi algpikkus
+    fps  : 1000 // Kaadreid sekundis (määrab liikumiskiiruse)
   };
   
-  // Merge Conf
+  // Konfiguratsiooni ühendamine (Merge Conf)
   if (typeof conf == 'object') {
     for (var key in conf) {
       if (conf.hasOwnProperty(key)) {
@@ -67,41 +67,43 @@ Component.Stage = function(canvas, conf) {
 };
 
 /**
- * Game Component Snake
+ * Mängukomponendi Uss (Snake)
  */
 Component.Snake = function(canvas, conf) {
   
-  // Game Stage
+  // Mängu lava loomine
   this.stage = new Component.Stage(canvas, conf);
   
-  // Init Snake
+  // Ussi initsialiseerimine
   this.initSnake = function() {
     
-    // Itaration in Snake Conf Size
+    // Loob ussi vastavalt algsuurusele
     for (var i = 0; i < this.stage.conf.size; i++) {
       
-      // Add Snake Cells
+      // Lisab ussi segmendid (x ja y koordinaadid)
       this.stage.length.push({x: i, y:0});
     }
   };
   
-  // Call init Snake
+  // Kutsu ussi initsialiseerimine
   this.initSnake();
   
-  // Init Food  
+  // Toidu initsialiseerimine  
   this.initFood = function() {
     
-    // Add food on stage
+    // Lisab toidu juhuslikule positsioonile laval
     this.stage.food = {
+      // Juhuslik x-koordinaat lava piirides (lahtri kaupa)
       x: Math.round(Math.random() * (this.stage.width - this.stage.conf.cw) / this.stage.conf.cw), 
+      // Juhuslik y-koordinaat lava piirides (lahtri kaupa)
       y: Math.round(Math.random() * (this.stage.height - this.stage.conf.cw) / this.stage.conf.cw), 
     };
   };
   
-  // Init Food
+  // Kutsu toidu initsialiseerimine
   this.initFood();
   
-  // Restart Stage
+  // Mängu taaskäivitamine
   this.restart = function() {
     this.stage.length            = [];
     this.stage.food              = {};
@@ -114,28 +116,28 @@ Component.Snake = function(canvas, conf) {
 };
 
 /**
- * Game Draw
+ * Mängu joonistamine
  */
 Game.Draw = function(context, snake) {
   
-  // Draw Stage
+  // Lava joonistamine (mängutsükkel)
   this.drawStage = function() {
     
-    // Check Keypress And Set Stage direction
+    // Kontrollib klahvivajutust ja määrab ussi uue suuna
     var keyPress = snake.stage.keyEvent.getKey(); 
     if (typeof(keyPress) != 'undefined') {
       snake.stage.direction = keyPress;
     }
     
-    // Draw White Stage
+    // Joonistab valge tausta
     context.fillStyle = "white";
     context.fillRect(0, 0, snake.stage.width, snake.stage.height);
     
-    // Snake Position
+    // Ussi pea praegused koordinaadid
     var nx = snake.stage.length[0].x;
     var ny = snake.stage.length[0].y;
     
-    // Add position by stage direction
+    // Arvutab uue pea positsiooni vastavalt suunale
     switch (snake.stage.direction) {
       case 'right':
         nx++;
@@ -151,74 +153,80 @@ Game.Draw = function(context, snake) {
         break;
     }
     
-    // Check Collision
+    // Kontrollib kokkupõrget (seinte või iseendaga)
     if (this.collision(nx, ny) == true) {
-      snake.restart();
+      snake.restart(); // Kokkupõrke korral taaskäivita mäng
       return;
     }
     
-    // Logic of Snake food
+    // Ussi toitumise loogika
     if (nx == snake.stage.food.x && ny == snake.stage.food.y) {
-      var tail = {x: nx, y: ny};
-      snake.stage.score++;
-      snake.initFood();
+      var tail = {x: nx, y: ny}; // Uus saba segment on uus pea positsioon
+      snake.stage.score++;       // Suurenda skoori
+      snake.initFood();          // Loo uus toit
     } else {
-      var tail = snake.stage.length.pop();
-      tail.x   = nx;
+      var tail = snake.stage.length.pop(); // Eemaldab viimase saba segmendi
+      tail.x   = nx;                     // Liigutab saba segmendi uuele pea positsioonile
       tail.y   = ny;  
     }
-    snake.stage.length.unshift(tail);
+    snake.stage.length.unshift(tail); // Lisab uue segmendi ussi massiivi algusesse (saab uueks peaks)
     
-    // Draw Snake
+    // Joonistab ussi
     for (var i = 0; i < snake.stage.length.length; i++) {
       var cell = snake.stage.length[i];
-      this.drawCell(cell.x, cell.y);
+      this.drawCell(cell.x, cell.y); // Joonistab iga ussi segmendi
     }
     
-    // Draw Food
+    // Joonistab toidu
     this.drawCell(snake.stage.food.x, snake.stage.food.y);
     
-    // Draw Score
+    // Joonistab skoori lõuendi alaossa
     context.fillText('Score: ' + snake.stage.score, 5, (snake.stage.height - 5));
   };
   
-  // Draw Cell
+  // Lahtri/segmendi joonistamine
   this.drawCell = function(x, y) {
-    context.fillStyle = 'rgb(170, 170, 170)';
+    context.fillStyle = 'rgb(170, 170, 170)'; // Hall värv
     context.beginPath();
+    // Joonistab ringi (ussil on ümarad punktid)
     context.arc((x * snake.stage.conf.cw + 6), (y * snake.stage.conf.cw + 6), 4, 0, 2*Math.PI, false);    
     context.fill();
   };
   
-  // Check Collision with walls
+  // Kontrollib kokkupõrget seina või iseendaga
   this.collision = function(nx, ny) {  
+    // Kontrollib, kas uus pea positsioon on väljaspool lava piire
     if (nx == -1 || nx == (snake.stage.width / snake.stage.conf.cw) || ny == -1 || ny == (snake.stage.height / snake.stage.conf.cw)) {
-      return true;
+      return true; // Kokkupõrge seinaga
     }
+    
+    // Kontrollib kokkupõrget iseendaga (pole siin koodis implementeeritud, aga oleks siin)
+    
     return false;    
   }
 };
 
 
 /**
- * Game Snake
+ * Mängu Uss initsialiseerimine
  */
 Game.Snake = function(elementId, conf) {
   
-  // Sets
-  var canvas   = document.getElementById(elementId);
-  var context  = canvas.getContext("2d");
-  var snake    = new Component.Snake(canvas, conf);
-  var gameDraw = new Game.Draw(context, snake);
+  // Seaded
+  var canvas   = document.getElementById(elementId); // Lõuendi element
+  var context  = canvas.getContext("2d");          // 2D joonistuskontekst
+  var snake    = new Component.Snake(canvas, conf); // Ussi objekti loomine
+  var gameDraw = new Game.Draw(context, snake);    // Joonistusobjekti loomine
   
-  // Game Interval
+  // Mängutsükli intervall (kutsub drawStage pidevalt välja)
   setInterval(function() {gameDraw.drawStage();}, snake.stage.conf.fps);
 };
 
 
 /**
- * Window Load
+ * Akna laadimisel käivitatav funktsioon
  */
 window.onload = function() {
+  // Loob uue Ussimängu eksemplari lõuendil 'stage' kiiruse 100ms ja algpikkusega 4
   var snake = new Game.Snake('stage', {fps: 100, size: 4});
 };
--- src/mäng2.js
+++ src/mäng2.js
@@ -1,46 +1,60 @@
-var mugHeight;
-var beerHeight;
-var percentFilled;
-var roundedPercent;
-var game = Math.floor((Math.random() * 100) + 1);
+/**
+ * Allikas/Autor: El Jefe (https://codepen.io/ElJefe/pen/vEERrW)
+ */
+var mugHeight;       // Kapa maksimaalne kõrgus
+var beerHeight;      // Õlle praegune kõrgus
+var percentFilled;   // Täituvus protsentides (ujukomaarv)
+var roundedPercent;  // Täituvus protsentides (ümaratuna)
+// Juhuslik sihtväärtus (protsent), mida mängija peab saavutama
+var game = Math.floor((Math.random() * 100) + 1); 
 
+// Funktsioon kõrguste ja täituvusprotsendi arvutamiseks/uuendamiseks
 function getHeights(){
 	mugHeight = $('#mug').height();
 	beerHeight = $('#beer').height();
 	percentFilled = (beerHeight / mugHeight) * 100;
   roundedPercent = Math.round(percentFilled);
+	// Uuendab HTML-i elemendis protsendi näidu
 	$('#percent-filled').html('Õlle kogus: ' + roundedPercent + '%');
 };
 
+// Kraani (handle) peale hiire liigutamise (hover) sündmus
 $('#handle').hover(
+  // mouseover (hiir liikus peale)
   function(){
-    $('#beer').addClass('fill');
-    $('#beer').css('animation-play-state', 'running');
-    $('#pour').addClass('pouring');
+    $('#beer').addClass('fill');                  // Lisab klassi, mis käivitab CSS animatsiooni
+    $('#beer').css('animation-play-state', 'running'); // Käivitab animatsiooni (õlle valamine)
+    $('#pour').addClass('pouring');               // Lisab klassi, mis muudab kraanist tuleva vedeliku nähtavaks
   },
+  // mouseout (hiir liikus ära)
   function(){
-    getHeights();
-    $('#beer').css('animation-play-state', 'paused');
-    $('#pour').removeClass('pouring');
+    getHeights();                                 // Arvutab lõpliku täituvusprotsendi
+    $('#beer').css('animation-play-state', 'paused'); // Peatab animatsiooni
+    $('#pour').removeClass('pouring');            // Peidab kraanist tuleva vedeliku
+    
+    // Kontrollib tulemust ja kuvab vastava teate
     if (roundedPercent === 0) {
+      // Ei tee midagi, kui valamine peatati kohe alguses
     } else if (roundedPercent === game) {
-    	$('#result').html('Edukas sooritus');
+    	$('#result').html('Edukas sooritus'); // Täpne täitmine
     } else if((game - roundedPercent) < 5 && (game - roundedPercent) > -5 ){
-      $('#result').html('Käib kahh');
+      $('#result').html('Käib kahh');         // Täitmine oli sihtmärgist +/- 4% piires
     } else {
-      $('#result').html('Ega ikka sitast saia saa');
+      $('#result').html('Häbi, häbi püksis käbi!'); // Ebaõnnestunud täitmine
     }
   }
 );
 
+// Kapa (mug) kliki sündmus (taaskäivitamine)
 $('#mug').click(function(){
-  $('#beer').removeClass('fill');
-  getHeights();
-  $('#result').html('');
-	game = Math.floor((Math.random() * 100) + 1);
-  $('#target').html(game);  
+  $('#beer').removeClass('fill');  // Eemaldab 'fill' klassi, nullides õlle taseme (CSS kaudu)
+  getHeights();                    // Arvutab uuesti kõrgused
+  $('#result').html('');           // Puhastab tulemuse teate
+	game = Math.floor((Math.random() * 100) + 1); // Loob uue juhusliku sihtväärtuse
+  $('#target').html(game);         // Uuendab sihtprotsendi näidu HTML-is
 })
 
+// Dokument on valmis (käivitub lehe laadimisel)
 $(document).ready(function(){
-	$('#target').html(game);  
+	$('#target').html(game);  // Määrab algse sihtprotsendi
 });
